be80f5fc1e194a6cb7bfd9e91eef680423f976e2
wyntuition <wynvandev@gmail.com>
2017-12-08 07:56:06 -0500

    upload tweak

--- upload-s3.sh
+++ upload-s3.sh
@@ -1,4 +1,12 @@
+### USAGE:
+# upload-s3 <bucket_name> <filename>
+
 S3_BUCKET=wbv
 FILENAME=w1.jpg
+
+if [ $# -ne 0 ] 
+    S3_BUCKET=$1
+    FILENAME=$2    
+fi
 
 aws s3 cp $FILENAME s3://$S3_BUCKET/$FILENAME
